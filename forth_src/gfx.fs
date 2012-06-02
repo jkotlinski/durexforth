1efeb02779429c52a578f65404c612e8ffc7341d
Johan Kotlinski <kotlinski@gmail.com>
2012-06-02 22:33:50 +0200

    added clrcol

--- forth_src/gfx.fs
+++ forth_src/gfx.fs
@@ -1,15 +1,22 @@
 
 
+: bmpbase a000 ;
+: colbase 8c00 ;
+
 :asm hires 
-# bmp 8000-9fff
-bb lda,# d011 sta,
-15 lda,# dd00 sta,
-0 lda,# d018 sta,
+bb lda,# d011 sta, # enable
+15 lda,# dd00 sta, # vic bank 2
+# bmp a000-bfff
+# col 8c00-9fff
+38 lda,# d018 sta,
 ;asm
 
-:asm nohires
-# revert
+:asm lores
 9b lda,# d011 sta,
 17 lda,# dd00 sta,
 17 lda,# d018 sta,
 ;asm
+
+: clrcol ( fgbgcol -- )
+colbase 3e8 fill
+0 gfxbase 1f40 fill ;
