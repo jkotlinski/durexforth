58b2539448f2a25567c16ca35fd6526426db679b
Johan Kotlinski <kotlinski@gmail.com>
2012-06-03 17:19:04 +0200

    horizontal line drawing

--- forth_src/gfx.fs
+++ forth_src/gfx.fs
@@ -28,10 +28,37 @@ create mask
 80 c, 40 c, 20 c, 10 c,
 8 c, 4 c, 2 c, 1 c,
 
+var penx var peny
+
 : plot ( x y -- )
+2dup peny ! penx !
 dup fff8 and 28 *
 swap 7 and + swap # y x
 dup 7 and ['] mask + c@ # y x bit
 -rot # bit y x
 fff8 and + bmpbase +
 dup c@ rot or swap c! ;
+
+var dx var dy
+
+: lineh
+dx @ 8000 and if
+# left
+begin dx @ while
+penx @ peny @ plot
+ffff penx +! 1 dx +!
+repeat
+else
+# right
+begin dx @ while
+penx @ peny @ plot
+1 penx +! ffff dx +!
+repeat
+then ;
+
+: line ( x y -- )
+peny @ - dy ! penx @ - dx !
+dy @ if
+else
+ lineh
+then ;
