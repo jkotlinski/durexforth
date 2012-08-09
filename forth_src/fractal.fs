bc11c72212f2c7d305f78a045d382bac8ed38147
Johan Kotlinski <kotlinski@gmail.com>
2012-08-09 12:53:47 +0200

    added bush1

--- forth_src/fractal.fs
+++ forth_src/fractal.fs
@@ -8,6 +8,8 @@ s" turtle" load
 : Dd 0 ; # delta distance
 var rule var rulel
 
+var stk
+
 : dofract ( depth -- depth )
 0 begin dup rulel < while
 dup rule + c@ case
@@ -18,10 +20,18 @@ else Dd forward then
 endof
 [ key + literal ] of Da right endof
 [ key - literal ] of Da left endof
+[ key [ literal ] of turtle@
+stk @ 2+ 2+ ! stk @ 2+ ! stk @ ! 6 stk +!
+endof
+[ key ] literal ] of
+6 negate stk +!
+stk @ @ stk @ 2+ @ stk @ 2+ 2+ @ turtle!
+endof
 endcase
 1+ repeat drop ;
 
 : fractal
+here @ stk !
 ( ax axl depth Dd Da rule rulel -- )
 to rulel to rule to Da to Dd
 0 # axiom axioml depth i
@@ -38,3 +48,6 @@ endcase
 : koch init 10 clrcol
 50 30 0 turtle!
 s" f++f++f" 4 2 3c s" f-f++f-f" fractal ;
+: bush1 init 50 clrcol
+a0 b0 10e turtle!
+s" f" 4 3 19 s" ff+[+f-f-f]-[-f+f+f]" fractal ;
