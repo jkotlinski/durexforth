7bdf6004aa83f640443c967ed0fa99018c45c230
Johan Kotlinski <kotlinski@gmail.com>
2012-10-17 01:42:30 +0200

    Added comment about rng.

--- forth_src/rnd.fs
+++ forth_src/rnd.fs
@@ -1,7 +1,9 @@
 
 
 var seed
-# Generates random number.
-# Poor code from Starting FORTH.
+# Random number generator from
+# Starting Forth. MSB has better
+# randomness than LSB, use 100/
+# when getting bytes.
 : rnd ( -- u ) seed @
 7abd * 1b0f + dup seed ! ;
