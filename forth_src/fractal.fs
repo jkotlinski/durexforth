a6384ac70745844dad4dcc476fc617b569eb6cd1
Johan Kotlinski <kotlinski@gmail.com>
2012-11-14 21:50:16 +0100

    Tidy up fractal

--- forth_src/fractal.fs
+++ forth_src/fractal.fs
@@ -4,20 +4,17 @@
 
 s" turtle" load
 
-: Da 0 ; # delta angle
-: Dd 0 ; # delta distance
-var rule var rulel
-var scale
+0 value Da # delta angle
+0 value Dd # delta distance
+
+var rule var rulel var scale
 
 : dofract ( depth -- depth )
 scale @ 100 <> if
-Dd dup >r scale @ *
-2/ 2/ 2/ 2/ 2/ 2/ 2/ 2/ to Dd
-then
+Dd dup >r scale @ * 100/ to Dd then
 0 begin dup rulel < while
 dup rule + c@ case
-[ key f literal ] of
-over if
+[ key f literal ] of over if
 swap 1- recurse 1+ swap
 else Dd forward then
 endof
@@ -27,14 +24,11 @@ endof
 [ key [ literal ] of turtle@ >r >r >r
 endof
 [ key ] literal ] of r> r> r> turtle!
-endof
-endcase
-1+ repeat drop
-scale @ 100 <> if r> to Dd then
-;
+endof endcase 1+ repeat drop
+scale @ 100 <> if r> to Dd then ;
 
-: fractal
-( ax axl depth scale Dd Da rule rulel -- )
+: fractal ( ax axl depth scale Dd Da
+rule rulel -- )
 to rulel to rule to Da to Dd scale !
 0 # axiom axioml depth i
 begin 2 pick over > while
@@ -56,10 +50,13 @@ s" f" 4 100 3 3c s" f-f++f-f" fractal
 s" f" 5 100 1 3c s" f-f++f-f" fractal ;
 : weed1 init d clrcol
 a0 c4 10e moveto
-s" f" 3 100 7 19 s" f[-f]f[+f]f" fractal ;
+s" f" 3 100 7 19 s" f[-f]f[+f]f"
+fractal ;
 : bush1 init d clrcol
 a0 bb 10e moveto
-s" f" 4 100 3 19 s" ff+[+f-f-f]-[-f+f+f]" fractal ;
+s" f" 4 100 3 19
+s" ff+[+f-f-f]-[-f+f+f]" fractal ;
 : bush2 init d clrcol d d020 c!
 a0 c8 10e moveto
-s" f" 6 80 64 14 s" @[+f]@[-f]+f" fractal ;
+s" f" 6 80 64 14
+s" @[+f]@[-f]+f" fractal ;
