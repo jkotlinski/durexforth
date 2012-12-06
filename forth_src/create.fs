a2a4fa46253196b7a8b6aab18b0e3129a0303792
Johan Kotlinski <kotlinski@gmail.com>
2012-12-06 04:22:56 +0100

    Simplified create, dodoes

--- forth_src/create.fs
+++ forth_src/create.fs
@@ -5,19 +5,17 @@
 pla, zptmp sta, pla, zptmp 1+ sta, tay,
 
 # Pushes data pointer to param stack.
-dex, dex, clc, zptmp lda, 3 adc,#
-0 sta,x 1 bcc, iny, 1 sty,x
+dex, dex,
+clc, zptmp lda, 3 adc,# 0 sta,x
+1 bcc, iny, 1 sty,x
 
-# Is behavior pointer null?
-2 ldy,# zptmp lda,(y)
-+branch beq, # Yes: done.
-
-# No: behavior pointer => IP
+# behavior pointer => IP
 ip lda, pha, ip 1+ lda, pha,
-zptmp lda,(y) ip 1+ sta, dey,
-zptmp lda,(y) ip sta,
-:+ ;asm
-    
-: create # 0 = behavior pointer
-header 20 c, ['] dodoes , 0 , ;
+2 ldy,# zptmp lda,(y) ip 1+ sta,
+dey,    zptmp lda,(y) ip sta,
+;asm
+
+here @ loc exit >cfa ,
+: create # default behavior = exit
+header 20 c, ['] dodoes , literal , ;
 : does> r> latest @ >dfa ! ;
