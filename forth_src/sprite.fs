af27812ff91523d8ff33b8964da7cc29f8566966
Johan Kotlinski <kotlinski@gmail.com>
2013-06-23 13:09:19 +0200

    More work on sprites

--- forth_src/sprite.fs
+++ forth_src/sprite.fs
@@ -16,8 +16,8 @@ else d010 clrbit then ;
 
 : sp-y! ( y n -- ) 2* d001 + c! ;
 
-: sp-show ( n -- ) d015 setbit ;
-: sp-hide ( n -- ) d015 clrbit ;
+: sp-xy! ( x y n -- )
+tuck sp-y! sp-x! ;
 
 ( expand width/height )
 : sp-1w ( n -- ) d01d clrbit ;
@@ -40,4 +40,33 @@ else d010 clrbit then ;
 : sp-on ( n -- ) d015 setbit ;
 : sp-off ( n -- ) d015 clrbit ;
 
-: sp-col! ( c n -- ) d027 + ! ;
+: sp-col! ( c n -- ) d027 + c! ;
+
+: sp-init
+( assume screen at $400, lowres,
+place sprites at 3e00-3fff )
+f8 7f8 ! f9 7f9 ! fa 7fa ! fb 7fb !
+fc 7fc ! fd 7fd ! fe 7fe ! ff 7ff ! ;
+
+: demo
+sp-init
+7 begin 
+dup sp-on
+1 over + over sp-col!
+?dup while 1- repeat
+
+18 33 0 sp-xy!
+18 19 + 33 1 sp-xy!
+18 19 2 * + 33 2 sp-xy!
+18 19 3 * + 33 3 sp-xy!
+18 19 4 * + 33 4 sp-xy!
+18 19 5 * + 33 5 sp-xy!
+18 19 6 * + 33 6 sp-xy!
+18 19 7 * + 33 7 sp-xy!
+
+begin
+1 7f8 +! 1 7f9 +! 1 7fa +! 1 7fb +!
+1 7fc +! 1 7fd +! 1 7fe +! 1 7ff +!
+again ;
+
+demo
