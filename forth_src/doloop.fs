1d10ebe1f9b916ee9f17d4ba3bf568de04e21e10
Johan Kotlinski <kotlinski@gmail.com>
2013-06-29 21:58:00 +0200

    Bugfixed do/loop

--- forth_src/doloop.fs
+++ forth_src/doloop.fs
@@ -7,13 +7,8 @@
 ' r> , ' 1+ , ' r> , ' 2dup , ' = ,
 [compile] until ' 2drop , ;
 
-: i immed [compile] r@ ;
+: i immed ' r@ , ;
 
-(
 : test 10 0 do i . loop ;
 
-cr see do
-cr see loop
-cr see test
 test
-)
