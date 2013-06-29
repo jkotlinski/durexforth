2480bbda439db9e7b115ba265c8941022260e9ec
Johan Kotlinski <kotlinski@gmail.com>
2013-06-29 22:05:31 +0200

    Added +loop

--- forth_src/doloop.fs
+++ forth_src/doloop.fs
@@ -7,8 +7,14 @@
 ' r> , ' 1+ , ' r> , ' 2dup , ' = ,
 [compile] until ' 2drop , ;
 
+: +loop immed
+' r> , ' + , ' r> , ' 2dup , ' >= ,
+[compile] until ' 2drop , ;
+
 : i immed ' r@ , ;
 
+(
 : test 10 0 do i . loop ;
 
 test
+)
