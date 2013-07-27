2b61f7456dd2c57c1622691d620c316b992f62ed
Johan Kotlinski <kotlinski@gmail.com>
2013-07-28 01:18:48 +0200

    change memory for charrom demo

--- forth_src/demo/charrom.fs
+++ forth_src/demo/charrom.fs
@@ -2,19 +2,17 @@
 
 ( how to modify char ROM font )
 : chardemo
-sei
-
 ( switch in char ROM )
+sei
 [ 2 to base ]
 1 c@ 11111000 and 11 or 1 c!
 
-( copy char ROM to $5800 )
-[ hex ] d800 5800 1000 cmove
+( copy char ROM to $7800 )
+[ hex ] d800 7800 1000 cmove
 
 ( switch back I/O + kernal )
 [ 2 to base ]
 1 c@ 11111000 and 110 or 1 c!
-
 cli
 
 ( set vic bank to $4000-$7fff )
@@ -22,10 +20,14 @@ cli
 [ 2 to base ] 11111100 and 10 or
 [ hex ] dd00 c!
 
+( set vic text screen = $7400,
+  vic character data = $7800 )
+de d018 c!
+
 ( fill text screen )
-4800 4400 do i dup c! loop
+7800 7400 do i dup c! loop
 
 ( invert font forever )
 begin
-6000 5800 do i c@ invert i c! loop
+8000 7800 do i c@ invert i c! loop
 again ;
