e6e2d7c52f6c23d9512100961c6341ec1de569da
Johan Kotlinski <kotlinski@gmail.com>
2013-07-29 12:29:01 +0200

    optimize do

--- forth_src/doloop.fs
+++ forth_src/doloop.fs
@@ -1,11 +1,17 @@
 
 
+:asm (do)
+2 lda,x pha, 3 lda,x pha,
+0 lda,x pha, 1 lda,x pha,
+inx, inx, inx, inx, ;asm
+
 : do ( limit first -- ) immed
-' swap , [compile] begin ' >r , ' >r , ;
+' (do) , [compile] begin ;
 
 : loop immed
-' r> , ' 1+ , ' r> , ' 2dup , ' = ,
-[compile] until ' 2drop , ;
+' r> , ' 1+ , ' r> , ' 2dup , ' < ,
+[compile] while ' >r , ' >r ,
+[compile] repeat ' 2drop , ;
 
 : +loop immed
 ' r> , ' + , ' r> , ' 2dup , ' >= ,
