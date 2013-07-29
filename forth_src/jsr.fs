15dd8bc3a9f9bf6e4b6cfb6ea2919a734ca9810c
Johan Kotlinski <kotlinski@gmail.com>
2013-07-29 23:54:35 +0200

    updated comment about basic CHRGET

--- forth_src/jsr.fs
+++ forth_src/jsr.fs
@@ -1,14 +1,16 @@
 
 
 var ar var xr var yr 
-( For BASIC CHRGET support:
+( For BASIC CHRGET support,
+  please include all out-
+  commented code lines!
+
   1d allot value jsrtmp )
 :asm jsr
 0 lda,x here 1+ 1234 sta, # lsb
 1 lda,x here 1+ 1234 sta, # msb
 txa, pha,
-( For BASIC CHRGET support:
-  1c ldx,# :-
+( 1c ldx,# :-
   73 lda,x jsrtmp sta,x
   e3a2 lda,x 73 sta,x
   dex, -branch bpl, )
@@ -16,8 +18,7 @@ ar lda, xr ldx, yr ldy,
 here 2+ swap ! here 1+ swap !
 1234 jsr,
 ar sta, xr stx, yr sty,
-( For BASIC CHRGET support:
-  1c ldx,# :-
+( 1c ldx,# :-
   jsrtmp lda,x 73 sta,x
   dex, -branch bpl, )
 pla, tax, inx, inx, ;asm
