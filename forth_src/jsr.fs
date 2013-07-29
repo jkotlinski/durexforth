1bc534254ea4982a24c6dd1b419742caeb57c030
Johan Kotlinski <kotlinski@gmail.com>
2013-07-30 00:01:40 +0200

    jsr now saves and restores CHRGET by default

--- forth_src/jsr.fs
+++ forth_src/jsr.fs
@@ -1,24 +1,19 @@
 
 
 var ar var xr var yr 
-( For BASIC CHRGET support,
-  please include all out-
-  commented code lines!
-
-  1d allot value jsrtmp )
 :asm jsr
 0 lda,x here 1+ 1234 sta, # lsb
 1 lda,x here 1+ 1234 sta, # msb
 txa, pha,
-( 1c ldx,# :-
-  73 lda,x jsrtmp sta,x
-  e3a2 lda,x 73 sta,x
-  dex, -branch bpl, )
+1c ldx,# :-
+73 lda,x 100 sta,x # save zp
+e3a2 lda,x 73 sta,x # restore CHRGET
+dex, -branch bpl,
 ar lda, xr ldx, yr ldy,
 here 2+ swap ! here 1+ swap !
 1234 jsr,
 ar sta, xr stx, yr sty,
-( 1c ldx,# :-
-  jsrtmp lda,x 73 sta,x
-  dex, -branch bpl, )
+1c ldx,# :-
+100 lda,x 73 sta,x # restore zp
+dex, -branch bpl,
 pla, tax, inx, inx, ;asm
