45835b8cbccb361014b34f8d8ff1b44dbabb8695
Johan Kotlinski <kotlinski@gmail.com>
2013-07-30 15:23:22 +0200

    Don't use $100 as temporary storage for jsr calls

--- forth_src/jsr.fs
+++ forth_src/jsr.fs
@@ -2,17 +2,18 @@
 
 ( Used to make Basic/Kernal calls.
   Variables ar, xr, yr are used for
-  register I/O. $100-$11c is used as
+  register I/O. T is used as
   temporary storage for the Forth
   zeropage, so that CHRGET can be in
   place for any Basic calls. )
 var ar var xr var yr 
+1d allot value T
 :asm jsr
 0 lda,x here 1+ 1234 sta, # lsb
 1 lda,x here 1+ 1234 sta, # msb
 txa, pha,
 1c ldx,# :-
-73 lda,x 100 sta,x # save zp
+73 lda,x T sta,x # save zp
 e3a2 lda,x 73 sta,x # restore CHRGET
 dex, -branch bpl,
 ar lda, xr ldx, yr ldy,
@@ -20,6 +21,7 @@ here 2+ swap ! here 1+ swap !
 1234 jsr,
 ar sta, xr stx, yr sty,
 1c ldx,# :-
-100 lda,x 73 sta,x # restore zp
+T lda,x 73 sta,x # restore zp
 dex, -branch bpl,
 pla, tax, inx, inx, ;asm
+hide T
