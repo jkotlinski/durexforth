c6fce46d72e4d1a4a3991d61344f18966bdbc687
Johan Kotlinski <kotlinski@gmail.com>
2013-07-31 12:18:48 +0200

    Fix up float.fs

--- forth_src/float.fs
+++ forth_src/float.fs
@@ -2,20 +2,22 @@
 
 : fac, bbca jsr
 57 here 5 cmove here 5 + to here ;
-
 # 5-byte float word from string
-: strfac ( str strlen -- )
+: strf ( str strlen -- )
 ar ! 22 ! b7b5 jsr create fac, ;
 # 5-byte float word from signed int
-: intfac ( s -- ) create
+: intf ( s -- ) create
 dup 100/ ar ! yr ! b391 jsr fac, ;
-# s" 3.14159" strfac pi
-# 4 intfac 4.
-# 8 negate intfac -8.
-: fac@ ( faddr -- )
+: fac! ( faddr -- )
 dup 100/ yr ! ar ! bba2 jsr ;
 : fac* ( faddr -- )
 dup 100/ yr ! ar ! ba28 jsr ;
 : fac. bddd jsr b487 jsr ab21 jsr ;
-# : test 4. fac@ -8. fac* fac. ;
 
+( example:
+
+s" 3.14159" strf pi
+2 negate intf -2.
+pi fac! -2. fac* fac.
+
+...prints -6.28318001 )
