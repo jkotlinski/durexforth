8a4d3e143922b56ced2602e4abcb5afe57dc3117
Johan Kotlinski <kotlinski@gmail.com>
2013-08-04 20:00:30 +0200

    make comment clearer

--- forth_src/doloop.fs
+++ forth_src/doloop.fs
@@ -10,7 +10,7 @@ inx, inx, inx, inx, ;asm
 
 :asm (loop)
 txa, tay, tsx, # x = stack pointer
-102 inc,x 3 bne, 101 inc,x # index++
+102 inc,x 3 bne, 101 inc,x # i++
 102 lda,x 104 cmp,x +branch bne,
 101 lda,x 103 cmp,x +branch bne,
 # loop done
