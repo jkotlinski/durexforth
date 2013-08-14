24e85842acdc6d31e2a3f74a31f0a5b7dd1aaf0d
Johan Kotlinski <kotlinski@gmail.com>
2013-08-14 20:03:54 +0200

    tidy up and comment labels

--- forth_src/labels.fs
+++ forth_src/labels.fs
@@ -1,24 +1,51 @@
 
 
-# asm local labels
+( asm local labels.
 
+n @: = label n
+n @@ = branch to label n
+
+...where n is in range[0, ff]
+
+relative branches are resolved by :asm
+- this allows for mixed forward and
+backward references, but it is not
+possible to branch over :asm
+
+-- example --
+:asm clrscr
+0 ldy,# tya, 1 @:
+400 sta,y 500 sta,y
+600 sta,y 700 sta,y
+dey, 1 @@ bne, ;asm )
+
+( refs and locs are arrays of
+2-byte address + 1-byte index )
 18 allot value refs # 8 refs
 f allot value locs # 5 locs
 var locp var refp
-: :asm :asm
-locs locp ! refs refp !
-0 refs 18 fill 0 locs f fill ;
-: @@
+
+locs locp ! refs refp ! # init
+0 refs 18 fill 0 locs f fill
+
+# reference
+: @@ ( index -- dummy )
 refp @ [ refs 18 + ] literal < assert
 here refp @ !
 2 refp +! refp @ c! 1 refp +! 0 ;
-: @: 
+# label
+: @: ( index -- )
 locp @ [ locs f + ] literal < assert
 here locp @ !
 2 locp +! locp @ c! 1 locp +! ;
-: ;asm ;asm
+: :asm :asm
 locs begin dup locp @ < while
 refs begin dup refp @ < while
 over 2+ c@ over 2+ c@ = if
 over @ over @ 2+ - over @ 1+ c!
-then 3 + repeat drop 3 + repeat drop ;
+then 3 + repeat drop 3 + repeat drop
+
+locs locp ! refs refp ! # reset
+0 refs 18 fill 0 locs f fill ;
+
+hide locs hide refs hide locp hide refp
