d6078daac3c46bd5df5d2071b94dbbcacb7b6fe5
Johan Kotlinski <kotlinski@gmail.com>
2014-04-05 11:41:59 +0200

    Newer, smaller jsr that reuses Kernal SYS method. 2nd attempt

--- forth_src/jsr.fs
+++ forth_src/jsr.fs
@@ -1,25 +1,22 @@
 
 
-( Used to make Basic/Kernal calls.
-  Variables ar, xr, yr are used for
-  register I/O. The Forth zeropage is
-  stored away so that CHRGET is in
-  place for any Basic calls. )
-var ar var xr var yr 
+( Calls Basic/Kernal routines.
+  Uses ar, xr, yr,sr for register I/O.
+  The Forth stack is temporarily 
+  stored away so that CHRGET is in 
+  place for Basic calls. )
+30c value ar 30d value xr
+30e value yr 30f value sr
 1d allot dup >r >r
-:asm jsr
-0 lda,x here 1+ 1234 sta, # lsb
-1 lda,x here 1+ 1234 sta, # msb
+:asm jsr ( addr -- )
+0 lda,x 14 sta, 1 lda,x 15 sta,
 txa, pha,
 1c ldx,# :-
 73 lda,x r> sta,x # save zp
 e3a2 lda,x 73 sta,x # restore CHRGET
 dex, -branch bpl,
-ar lda, xr ldx, yr ldy,
-here 2+ swap ! here 1+ swap !
-1234 jsr,
-ar sta, xr stx, yr sty,
-1c ldx,# :-
+e130 jsr, # perform [sys]
+1c ldx,# :- 
 r> lda,x 73 sta,x # restore zp
 dex, -branch bpl,
 pla, tax, inx, inx, ;asm
