68baeb5ad0829b247653e9bebf08f583395a76a1
Johan Kotlinski <kotlinski@gmail.com>
2014-04-13 09:44:07 +0200

    another song that sounds more promising

--- forth_src/music.fs
+++ forth_src/music.fs
@@ -135,11 +135,12 @@ repeat drop ;
 
 : play-melody ( str strlen -- )
 to strlen to str
-f sid-vol 10 ctl! 9 srad! 4 o
+f sid-vol 
+10 ctl! 9 srad!
 play ;
 
 : music
-# s" l64c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&c&cr1<f8l16arararf8arararf8arararf8ararare8grgrgre8grgrgre8grgrgre8grgrgrf8arararf8arararf8arararf8ararare8grgrgre8grgrgre8grgrgre8grgrgrdrfrdrfr<gr>dr<gr>grcrgrcrgr<ar>er<a>rardrfrdrfr<gr>dr<gr>grcrgrcrgr<ar>er<a>rardrfrdrfr<gr>dr<gr>grcrgrcrgr<ar>er<a>rardrfrfr8.drarar8.crbrbr8.crbrbr8.erar8.arerar8.arerg+rg+rerer8.er8."
-# s" r1r1r8v10l16crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcr8.crcrcrl8def4gab4b+be2&e.l32&erd8e16.rf4g8a16.rb4>c8d16.re2&e8.&er<d8e16.rf4g8a16.rb4b+8b16.re2&e8.&erl8dcfegfagbab+b>dcl16ef8de1r4.<<g+rg+r8.g+r8."
-s" r1r1v15l8>fab4fab4fab>ed4c-c<b8ge2&e8d8ege2&e8.&e16fab4fab4fab>ed4c-ce8<bg2&g8bg8de2&e8.&e16r1r1f8ga4b8>cd4e8fg2&g8.&g32r1r1r32<f8ea8gb8ab+8b>d8ce8df8ec-16c<a16b1r1"
+s" v10l16o3f8o4crcrcro3f8o4crcrcro3f8o4crcrcro3f8o4crcro3cre8o4crcrcro3e8o4crcrcro3e8o4crcrcro3e8o4crcro3c8f8o4crcrcro3f8o4crcrcro3f8o4crcrcro3f8o4crcro3cro3e8o4crcrcro3e8o4crcrcro3e8o4crcrcro3e8o4crcrc8o3drardraro2gro3gro2gro3grcro4cro3cro4cro2aro3aro2aro3aro3drardraro2gro3gro2gro3grcro4cro3cro4cro2aro3aro2aro3aro3drardraro2gro3gro2gro3grcro4cro3cro4cro2aro3aro2aro3aro3drararrrdrararrrcrbrbrrrcrbrbrrrerarrrarerarrrarerg+rg+rg+rg+rrre&v5er"
+s" v14l16o5frarb4frarb4frarbr>erd4<b8>cr<brgre2&e8drergre2&e4frarb4frarb4frarbr>erd4<b8>crer<brg2&g8brgrdre2&e4r1r1frgra4br>crd4e8frg2&g4r1r1<f8era8grb8ar>c8<br>d8cre8drf8er<b>cr<ab1&b2r4e&v8e&v5er"
+s" v14l16r1r1r1r1r1r1r1r1o4drerf4grarb4>c8<bre2&e4drerf4grarb4>c8dre2&e4<drerf4grarb4>c8<bre2&e4d8crf8erg8fra8grb8ar>c8<br>d8crefrde1&e2r4"
 play-melody ;
