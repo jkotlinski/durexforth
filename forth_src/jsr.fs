2dd40b6e66ea568d232f762a6dc7a173a514823a
Johan Kotlinski <kotlinski@gmail.com>
2015-03-27 01:05:29 +0100

    simplified jsr

--- forth_src/jsr.fs
+++ forth_src/jsr.fs
@@ -7,16 +7,8 @@
   place for Basic calls. )
 30c value ar 30d value xr
 30e value yr 30f value sr
-1d allot dup >r >r
 :asm jsr ( addr -- )
 sp0 lda,x 14 sta, sp1 lda,x 15 sta,
 txa, pha,
-1c ldx,# :-
-73 lda,x r> sta,x # save zp
-e3a2 lda,x 73 sta,x # restore CHRGET
-dex, -branch bpl,
 e130 jsr, # perform [sys]
-1c ldx,# :- 
-r> lda,x 73 sta,x # restore zp
-dex, -branch bpl,
 pla, tax, inx, ;asm
