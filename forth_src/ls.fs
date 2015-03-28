1773700aaeec7cc392d2ac0203aeba3663018d7a
Johan Kotlinski <kotlinski@gmail.com>
2015-03-28 01:23:58 +0100

    further size optimize ls

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -2,16 +2,11 @@
 
 # submitted by kevin reno
 
-: to0 begin
-dup c@ dup if
-emit 1+ else drop 1+ cr exit
-then again ;
-
 : ls base decimal
 s" $" here loadb drop
 ae @ 2 - here 
-begin
-2dup <> while 
+begin 2dup <> while 
 2+ dup @ . space 2+
-to0 repeat
+begin dup c@ ?dup while
+emit 1+ repeat 1+ cr repeat
 2drop to base ;
