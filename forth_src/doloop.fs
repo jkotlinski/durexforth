78c71243876ad4ad3f325f1af031a03639eac6de
Johan Kotlinski <kotlinski@gmail.com>
2016-01-04 10:21:21 +0100

    another +loop fix

--- forth_src/doloop.fs
+++ forth_src/doloop.fs
@@ -51,9 +51,14 @@ zptmp2 (jmp),
 postpone (loop) dup , resolve-leaves ;
 
 : (+loop) ( inc -- )
-r> swap r> over + dup rot
-0< if r@ < else r@ 1- > then
-0= if >r >r [ ' branch jmp, ] then
+r> swap r> 2dup + rot
+0< if \ negative inc
+tuck r@ < swap r@ 1- >
+else \ positive inc
+tuck r@ 1- > swap r@ < 
+then
+and 0= if
+>r >r [ ' branch jmp, ] then
 r> 2drop 2+ >r ;
 
 : +loop immediate no-tce
