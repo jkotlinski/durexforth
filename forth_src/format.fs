d83a090cd925e6e6cd958bcbeb8e4b276652869f
Johan Kotlinski <kotlinski@gmail.com>
2016-01-04 12:30:05 +0100

    fixed # for double values

--- forth_src/format.fs
+++ forth_src/format.fs
@@ -6,5 +6,8 @@ here dup 1+ end @ here - cmove>
 1 end +! ;
 : hold ins here c! ;
 : sign 0< if [char] - hold then ;
-: # base @ um/mod swap #pet hold 0 ;
+: ud/mod \ from Gforth
+>r 0 r@ um/mod r> swap >r
+um/mod r> ;
+: # base @ ud/mod rot #pet hold ;
 : #s begin 2dup or while # repeat ;
