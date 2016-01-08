61cdbfa66d21cc05c989ba783aac2ce645e5b9f1
Johan Kotlinski <kotlinski@gmail.com>
2016-01-09 00:32:00 +0100

    test uppercase chars

--- forth_src/testcoreplus.fs
+++ forth_src/testcoreplus.fs
@@ -216,6 +216,7 @@ T{ %10010110 -> 150 }T
 T{ %-10010110 -> -150 }T
 \ T{ %-10010110. -> -150. }T
 T{ 'z' -> char z }T
+T{ 'Z' -> char Z }T
 \ Check BASE is unchanged
 T{ BASE @ OLD-BASE @ = -> <TRUE> }T
 
@@ -234,6 +235,7 @@ T{ %10010110 -> 96 }T
 T{ %-10010110 -> -96 }T
 \ T{ %-10010110. -> -96. }T
 T{ 'z' -> char z }T
+T{ 'Z' -> char Z }T
 \ Check BASE is unchanged
 T{ BASE @ OLD-BASE @ = -> <TRUE> }T   \ 2
 
