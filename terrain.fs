cd9d039afa429d1b975fa5520d0544e0652a45de
Johan Kotlinski <kotlinski@gmail.com>
2018-01-27 13:39:01 +0100

    works?

--- terrain.fs
+++ terrain.fs
@@ -1,8 +1,8 @@
 decimal
 17 constant w
 w dup * constant mapsize
-here mapsize allot constant map
-map mapsize 0 fill \ debug only
+here mapsize allot constant m \ map
+m mapsize 0 fill \ debug only
 
 256 value r \ range
 16 value s \ step size
@@ -11,13 +11,13 @@ map mapsize 0 fill \ debug only
 : c>d dup $80 and if $ff00 or then ;
 
 \ init endpoints
-crnd map c! \ nw
-crnd map w 1- + c! \ ne
-crnd map w dup * + 1- c! \ se
-crnd map w dup 1- * + c! \ sw
+crnd m c! \ nw
+crnd m w 1- + c! \ ne
+crnd m w dup * + 1- c! \ se
+crnd m w dup 1- * + c! \ sw
 
 : diamond  ( x y -- )
-w * + map + dup \ nw nw
+w * + m + dup \ nw nw
 dup s + \ nw nw ne
 dup s w * + \ nw nw ne se
 dup s - \ nw nw ne se sw
@@ -25,12 +25,43 @@ c@ c>d swap c@ c>d + swap
 c@ c>d + swap c@ c>d + 4 / \ nw avg
 crnd + swap \ val nw
 s 2/ + s 2/ w * + c! ;
-
 : diamonds
 w 1- 0 do w 1- 0 do
-i j diamond
+i j diamond s +loop s +loop ;
+
+variable x
+variable y
+variable n
+: square ( x y -- )
+2dup . . cr
+y ! x ! 0 n ! 0 \ sum
+\ sample up
+y @ s 2/ - -1 > if
+x @ y @ s 2/ - w * m + + c@ c>d +
+1 n +! then
+\ sample down
+y @ s 2/ + w < if
+x @ y @ s 2/ + w * m + + c@ c>d +
+1 n +! then
+\ sample left
+x @ s 2/ - -1 > if
+x @ s 2/ - y @ w * m + + c@ c>d +
+1 n +! then
+\ sample right
+x @ s 2/ + w < if
+x @ s 2/ + y @ w * m + + c@ c>d +
+1 n +! then
+n @ / crnd +
+m x @ + y @ w * + c! ;
+
+: squares
+w 1- 0 do w 1- 0 do
+i s 2/ + j square \ u
+i s 2/ + j s + square \ d
+i j s 2/ + square \ l
+i s + j s 2/ + square \ r
 s +loop s +loop ;
-: squares ;
+
 : diamond-square
 begin
 s 1 > while
@@ -44,7 +75,7 @@ diamond-square
 w 0 do w 0 do
 j 15 * 64 + i -3 * + \ x
 i 8 * 50 + \ y
-map i w * j + + c@ c>d 2/ 2/ + \ yd
+m i w * j + + c@ c>d 2/ 2/ + \ yd
 plot loop loop ;
 
 hex 52 clrcol hires
