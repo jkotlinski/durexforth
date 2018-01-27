70e5ea1c2f4036a3b0c8578e3c426e282c16173b
Johan Kotlinski <kotlinski@gmail.com>
2018-01-27 10:36:54 +0100

    3d plot

--- terrain.fs
+++ terrain.fs
@@ -1,5 +1,5 @@
 decimal
-33 constant w
+15 constant w
 w dup * constant mapsize
 here mapsize allot constant map
 map mapsize 0 fill
@@ -10,12 +10,18 @@ rnd map w 1- + c! \ ne
 rnd map w dup * + 1- c! \ se
 rnd map w dup 1- * + c! \ sw
 
-: plot-2d
+( : plot-2d
 w 0 do w 0 do
 j w * i + map + c@ if
 i j plot then
-loop loop ;
+loop loop ; )
+
+: plot-3d
+w 0 do w 0 do
+j 15 * 88 + i -5 * + \ x
+i 8 * 50 + \ y
+plot loop loop ;
 
 hex 52 clrcol hires
-plot-2d
+plot-3d
 key quit
