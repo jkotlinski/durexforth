5616ac9ffc0078423492a0e729cdda547d5bb2ad
Johan Kotlinski <kotlinski@gmail.com>
2018-01-27 10:56:33 +0100

    add range, stepsize

--- terrain.fs
+++ terrain.fs
@@ -4,13 +4,24 @@ w dup * constant mapsize
 here mapsize allot constant map
 map mapsize 0 fill
 
-: crnd rnd 256 / ;
+variable range 256 range !
+variable stepsize 32 stepsize !
+
+: crnd rnd range @ / ;
 
 \ init endpoints
 crnd map c! \ nw
 crnd map w 1- + c! \ ne
 crnd map w dup * + 1- c! \ se
 crnd map w dup 1- * + c! \ sw
+
+: diamond-square
+begin
+stepsize @ 1 > while
+stepsize @ 2/ stepsize !
+range @ 2* range !
+repeat ;
+diamond-square
 
 : c>d dup $80 and if $ff00 or then ;
 
