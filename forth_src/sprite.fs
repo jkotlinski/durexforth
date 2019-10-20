e7583ac828e52bbb17ad9cb649677e0451a4ea30
Johan Kotlinski <kotlinski@gmail.com>
2019-10-20 15:17:14 +0200

    made sprite.fs not mess with BASE

--- forth_src/sprite.fs
+++ forth_src/sprite.fs
@@ -1,6 +1,5 @@
-hex
 here
-80 c, 40 c, 20 c, 10 c,
+$80 c, $40 c, $20 c, $10 c,
 8 c, 4 c, 2 c, 1 c,
 : 80lsr literal + c@ ;
 : setbit ( n addr -- )
@@ -9,11 +8,11 @@ swap 80lsr over c@ or swap c! ;
 swap 80lsr invert over c@ and swap c! ;
 
 : sp-x! ( x n -- )
-2dup 2* d000 + c! \ lsb
-swap 100 and if d010 setbit
-else d010 clrbit then ;
+2dup 2* $d000 + c! \ lsb
+swap $100 and if $d010 setbit
+else $d010 clrbit then ;
 
-: sp-y! ( y n -- ) 2* d001 + c! ;
+: sp-y! ( y n -- ) 2* $d001 + c! ;
 
 : sp-xy! ( x y n -- )
 tuck sp-y! sp-x! ;
@@ -21,15 +20,15 @@ tuck sp-y! sp-x! ;
 : 7s- 7 swap - ;
 
 ( expand width/height )
-: sp-1w ( n -- ) 7s- d01d clrbit ;
-: sp-2w ( n -- ) 7s- d01d setbit ;
-: sp-1h ( n -- ) 7s- d017 clrbit ;
-: sp-2h ( n -- ) 7s- d017 setbit ;
+: sp-1w ( n -- ) 7s- $d01d clrbit ;
+: sp-2w ( n -- ) 7s- $d01d setbit ;
+: sp-1h ( n -- ) 7s- $d017 clrbit ;
+: sp-2h ( n -- ) 7s- $d017 setbit ;
 
-: sp-on ( n -- ) 7s- d015 setbit ;
-: sp-off ( n -- ) 7s- d015 clrbit ;
+: sp-on ( n -- ) 7s- $d015 setbit ;
+: sp-off ( n -- ) 7s- $d015 clrbit ;
 
-: sp-col! ( c n -- ) d027 + c! ;
+: sp-col! ( c n -- ) $d027 + c! ;
 
 ( read sprite byte )
 : ks
