9f89ee456219df0246ed04916ecd6a5f193d8168
Johan Kotlinski <kotlinski@gmail.com>
2019-10-23 17:12:12 +0200

    move REFILL

--- io.asm
+++ io.asm
@@ -20,7 +20,7 @@
 ;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ;THE SOFTWARE. }}}
 
-; TYPE EMIT PAGE KEY? KEY
+; TYPE EMIT PAGE KEY? KEY REFILL
 
     +BACKLINK
     !byte	4
@@ -78,4 +78,97 @@ TYPE ; ( caddr u -- )
     ldx W
     ldy #0
     jmp pushya
+
+    +BACKLINK
+    !byte	6
+    !text	"refill" ; ( -- )
+REFILL
+
+READ_EOF = * + 1
+    lda #0
+    beq +
+    ; handle EOF
+    stx tmp_x
+    lda	SOURCE_ID_LSB
+    jsr	CLOSE
+    dec	SOURCE_ID_LSB
+    ldx SOURCE_ID_LSB
+    jsr CHKIN
+    ldx tmp_x
+    jmp RESTORE_INPUT ; exit
++
+    lda SOURCE_ID_MSB
+    beq +
+    jsr evaluate_consume_tib
+    jmp evaluate_get_new_line
++
+    ldy #0
+    sty TO_IN_W
+    sty TO_IN_W + 1
+    sty TIB_SIZE
+    sty TIB_SIZE + 1
+
+    lda SOURCE_ID_LSB
+    beq .getLineFromConsole
+    lda SOURCE_ID_MSB
+    beq	.getLineFromDisk
+
+.getLineFromConsole
+    stx tmp_x
+    ldx #0
+-   jsr $e112 ; Input Character
+    cmp #$d
+    beq .gotReturn
+    sta TIB,x
+    cpx #$58 ; Default TIB area is $200-$258
+    beq -
+    inx
+    jmp -
+.gotReturn
+    jsr PUTCHR
+    ; Set TIB_SIZE to number of chars fetched.
+    stx TIB_SIZE
+    ldx tmp_x
+    rts
+
+.getLineFromDisk
+    lda TIB_PTR
+    sta W
+    lda TIB_PTR + 1
+    sta W + 1
+-   jsr GET_CHAR_BLOCKING
+    cmp #K_RETURN
+    beq +
+    inc $d020
+    ldy TIB_SIZE
+    sta (W),y
+    inc TIB_SIZE
+    dec $d020
+    jmp -
++   rts
+
+GET_CHAR_FROM_TIB
+    lda TO_IN_W
+    cmp TIB_SIZE
+    bne +
+    lda TO_IN_W + 1
+    cmp TIB_SIZE + 1
+    bne +
+    lda #0
+    rts
++
+    clc
+    lda TIB_PTR
+    adc TO_IN_W
+    sta W
+    lda TIB_PTR + 1
+    adc TO_IN_W + 1
+    sta W + 1
+    ldy #0
+    lda (W),y
+
+    inc TO_IN_W
+    bne +
+    inc TO_IN_W + 1
++   rts
 
