41efead748933b7c83360c1ad5a629f521d76109
Johan Kotlinski <kotlinski@gmail.com>
2020-03-10 21:47:43 +0100

    fix: required marked not found files as included

--- forth_src/require.fs
+++ forth_src/require.fs
@@ -1,13 +1,16 @@
+\ ! required stops working after
+\ 16 includes
 variable req $1e allot
 req $20 0 fill
 : hash ( addr u -- hash )
 over + swap 0 -rot
 do $1f * i c@ + loop ;
 : included ( addr u -- )
+2dup hash -rot included
 req $20 + req do 
-2dup hash i @ = if leave then
-i @ 0= if 2dup hash i ! leave then
-2 +loop included ;
+i @ 0= if i ! leave then
+dup i @ = if drop leave then
+2 +loop ;
 : include bl word count included ;
 
 : required ( addr u -- )
