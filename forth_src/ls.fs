abf3ebe7129af2cc3b22d4357a99f376e8c26d3c
Whammo <38296423+Whammo@users.noreply.github.com>
2020-04-12 19:14:39 -0700

    Update ls.fs

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -1,7 +1,26 @@
 \ submitted by kevin reno
 
-: ls s" $" here loadb 2 - here
+: ls
+$f word count dup 0<> if
+tuck here 3 + swap move
+3 + here
+'$' over c!
+'0' over 1+ c!
+':' over 2+ c!
+tuck
+else 2drop s" $" here then
+loadb 2 - here
 begin 2dup <> while
 2+ dup @ . space 2+
 begin dup c@ ?dup while
-emit 1+ repeat 1+ cr repeat 2drop ;
+emit 1+ repeat 1+ cr
+more repeat 2drop ;
+
+
+
+
+
+
+
+
+
