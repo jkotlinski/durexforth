2aa3f33f8fecca90deceec73ad5bb635ba8f45d8
Whammo <38296423+Whammo@users.noreply.github.com>
2020-04-16 02:21:57 -0700

    Update ls.fs
    Fixed my comments.

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -1,11 +1,11 @@
 \ submitted by kevin reno
 
 : ls ( addr -- )
-begin ?dup while        / abort if addr = 0
-2+ dup @ . 2+           / skip line link, print blocks
-begin dup c@ ?dup while / eol?
-emit 1+ repeat 1+ cr    / print line, eol
-dup c@ 0= if c@ then    / if eof then addr = 0
+begin ?dup while        \ abort if addr = 0
+2+ dup @ . 2+           \ skip line link, print blocks
+begin dup c@ ?dup while \ eol?
+emit 1+ repeat 1+ cr    \ print line, eol
+dup c@ 0= if c@ then    \ if eof then addr = 0
 repeat ;                
 
 \ sample code
