07779a311c112bbe40becba0e3f03d038bd6503d
Whammo <38296423+Whammo@users.noreply.github.com>
2020-04-25 03:46:50 -0700

    Update ls.fs
    ls to operate the same but with optional wildcards.
    Spun off rdir and added bs to demonstrate.

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -1,6 +1,6 @@
 \ submitted by kevin reno
 
-: ls ( addr -- )
+: rdir ( addr -- )
 begin ?dup while        \ abort if addr = 0
 2+ dup @ . 2+           \ skip line link, print blocks
 begin dup c@ ?dup while \ eol?
@@ -8,10 +8,11 @@ emit 1+ repeat 1+ cr    \ print line, eol
 dup c@ 0= if c@ then    \ if eof then addr = 0
 repeat ;                
 
-\ sample code
-\ $c000 try $0:*=s
-\ : try ( addr -- ) 
-\ dup parse-name rot loadb drop ls ;
+: ls parse-name ?dup if  \ accepts wildcards or not 
+else drop s" $"  
+then here loadb drop here rdir ;
+
+: bs here rdir ;  \ careful, if pad has changed
 
 
 
