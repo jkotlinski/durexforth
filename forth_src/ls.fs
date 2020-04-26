3a585d5ee442197f4c6d0f93b4d2d221a4bfa967
Whammo <38296423+Whammo@users.noreply.github.com>
2020-04-26 15:30:21 -0700

    Update ls.fs
    Dropped MORE into LS

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -2,7 +2,7 @@
 
 : rdir ( addr -- )
 begin ?dup while        \ abort if addr = 0
-2+ dup @ . 2+           \ skip line link, print blocks
+more 2+ dup @ . 2+      \ skip line link, print blocks
 begin dup c@ ?dup while \ eol?
 emit 1+ repeat 1+ cr    \ print line, eol
 dup c@ 0= if c@ then    \ if eof then addr = 0
