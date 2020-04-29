5a41dbce91d7e085908a17a9b378a39a1415a4ba
Whammo <38296423+Whammo@users.noreply.github.com>
2020-04-28 21:14:11 -0700

    Migrate from HERE to PAD
     Almost 100% certain that despite the comma, FAC, is not a compiling word.
    If it is now pointed to PAD perhaps this convention may be dropped.
    Please correct me if I'm wrong.

--- forth_src/float.fs
+++ forth_src/float.fs
@@ -15,7 +15,7 @@ s" .8" strf .8
 : bsys \ system call to BASIC ROM
 1 c@ dup 3 or 1 c! swap sys 1 c! ;
 : fac, $bbca bsys
-$57 here 5 move here 5 + to here ;
+$57 pad 5 move pad 5 + to pad ;
 \ 5-byte float word from string
 : strf ( str strlen -- )
 ar ! $22 ! $b7b5 bsys create fac, ;
