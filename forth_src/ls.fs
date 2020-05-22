19a3f6016b997f0f0af1bb75b9d947062682c073
Whammo <38296423+Whammo@users.noreply.github.com>
2020-05-22 14:35:06 -0700

    Comments are good, long lines, not so much.

--- forth_src/ls.fs
+++ forth_src/ls.fs
@@ -1,12 +1,12 @@
 \ submitted by kevin reno
 
 : rdir ( addr -- )
-begin ?dup while        \ abort if addr = 0
-more 2+ dup @ . 2+      \ skip line link, print blocks
-begin dup c@ ?dup while \ eol?
-emit 1+ repeat 1+ cr    \ print line, eol
-dup c@ 0= if c@ then    \ if eof then addr = 0
-repeat ;                
+begin ?dup while
+more 2+ dup @ . 2+
+begin dup c@ ?dup while 
+emit 1+ repeat 1+ cr
+dup c@ 0= if c@ then
+repeat ;
 
 : ls parse-name ?dup if
 else drop s" $"
