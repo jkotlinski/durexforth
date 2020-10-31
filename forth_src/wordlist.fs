c2d2d2e93a2406035f696f01220a2155f390b9e6
Poindexter Frink <sliderule@gmail.com>
2020-10-31 19:15:57 -0400

    DEFINE assigns new behaviour to a word

---
+++ forth_src/wordlist.fs
@@ -0,0 +1,12 @@
+: hide ( "name" -- )
+parse-name find-name ?dup if
+dup latest @ - ( nt size )
+>r c@ $1f and 3 + ( off )
+latest @ swap over +  ( srca dsta )
+dup latest !
+r> move then ;
+: define ( "name" -- )
+parse-name find-name ?dup 0=
+abort" not found."
+count $1f and + here swap !
+1 state ! 0 ;
