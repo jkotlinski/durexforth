a5a3dd9f2751fac449e2c5a369266b553f1bcf41
Johan Kotlinski <kotlinski@gmail.com>
2020-12-07 23:25:36 +0100

    document and tidy up turtle

--- forth_src/turtle.fs
+++ forth_src/turtle.fs
@@ -1,4 +1,18 @@
+header init     ( -- )
+header forward  ( px -- )
+header back     ( px -- )
+header left     ( deg -- )
+header right    ( deg -- )
+header penup    ( -- )
+header pendown  ( -- )
+header turtle@  ( -- x y deg )
+header turtle!  ( x y deg -- )
+header moveto   ( x y deg -- )
+
+require gfx
 require sin
+
+latest
 
 variable tx variable ty \ 10.6 fixedpoint
 variable ta 0 value tp
@@ -7,40 +21,43 @@ variable ta 0 value tp
 2/ dup $4000 and if $8000 or then ;
 : ls 2* 2* 2* 2* 2* 2* ;
 : rs s2/ s2/ s2/ s2/ s2/ s2/ ;
-: pendown 1 to tp tx @ rs ty @ rs plot ;
-: penup 0 to tp ;
+define pendown
+1 to tp tx @ rs ty @ rs plot ;
+define penup 0 to tp ;
 
-: moveto ( x y a -- )
+define moveto
 ta ! ls ty ! ls tx !
 pendown ;
-: init hires 7 clrcol
+define init hires 7 clrcol
 $a0 $64 $10e moveto ;
 
-: right ( a -- )
+define right
 ta @ +
 dup $8000 and if $168 + then
 $168 mod ta ! ;
-: left ( a -- )
+define left
 negate right ;
-: forward ( px -- )
+define forward
 ls dup ta @ *cos tx +!
 ta @ *sin ty +!
 tp if tx @ rs ty @ rs line then ;
-: back ( px -- )
+define back
 $80 right forward $80 right ;
 
-: turtle@ ( -- x y angle )
+define turtle@
 tx @ ty @ ta @ ;
-: turtle! ( x y angle -- )
+define turtle!
 ta ! ty ! tx ! pendown ;
 
+to latest
+
 (
-\ --- demo
+\ --- demo - not working :(
 
 : polyspiral
-." init distance? " interpret 
+." init distance? " interpret
 ." angle? " interpret
-." distance step? " interpret 
+." distance step? " interpret
 init
 $64 >r begin
 2 pick forward
@@ -50,9 +67,9 @@ r> 1- dup >r 0= until r> 2drop 2drop
 5 $d020 c! key drop lores 0 $d020 c! ;
 
 : inward
-." distance? " interpret 
+." distance? " interpret
 ." init angle? " interpret
-." angle step? " interpret 
+." angle step? " interpret
 init
 begin 2 pick forward
 over right
