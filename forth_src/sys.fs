d6d7d259a3253eca85a9d6b01a6e3a2297c06345
Whammo <38296423+Whammo@users.noreply.github.com>
2022-01-04 03:46:44 -0800

    Update sys.fs
    #288

--- forth_src/sys.fs
+++ forth_src/sys.fs
@@ -7,3 +7,24 @@ lsb lda,x $14 sta, msb lda,x $15 sta,
 txa, pha,
 $e130 jsr, \ perform [sys]
 pla, tax, inx, ;code
+
+code bank ( u -- )
+lsb     lda,x
+w       sta,
+$01     lda,
+$f8     and,#
+w       ora,
+$01     sta,
+        inx,
+;code        
+
+\ bank  A000  D000  D800  E000
+\ 0     RAM   RAM   RAM   RAM
+\ 1     RAM   CHARG CHARG RAM
+\ 2     RAM   CHARG CHARG ROM
+\ 3     ROM   CHARG CHARG ROM
+\ 4     RAM   RAM   RAM   RAM
+\ 5     RAM   I/O   NYBLE RAM
+\ 6     RAM   I/O   NYBLE ROM
+\ 7     ROM   I/O   NYBLE ROM
+
