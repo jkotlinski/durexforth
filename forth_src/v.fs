jkotlinski/durexforth

commit 6ea629953f394f8230d36ddeff8488319b3a55c6

Move V buffer to $a000-$cbfff, check if file too big (#395)
* before loading file in v, check if its too big

* save a few bytes by inlining

* fix stack underflow

* moved V text buffer to $a000

* rewrite some code in asm to save space

* explicit decimal

* edit changelog

* save a few bytes

* fix size check for file not found

* edit changelog

forth_src/v.fs
@@ -6,7 +6,7 @@ latest \ begin hiding words
 
 $d value lf
 
-$7001 value bufstart
+$a001 value bufstart \ use $a000-$cbff
 
 \ eof points to 0 sentinel
 variable eof ( ram eof )
@@ -369,7 +369,7 @@ here
 's' over c! 1+
 '0' over c! 1+
 ':' over c! 1+
-filename 1+ over filename c@ move
+filename count swap -rot move
 filename c@ + lf swap c!
 here filename c@ 4 +
 $f $f open ioabort $f close
@@ -651,13 +651,22 @@ eof @ if \ something in buffer?
 2drop main-loop exit \ yes - cont. edit
 then then
 
-2dup filename c! filename 1+ $f move
+filename c! filename 1+ $f move
 
 reset-buffer
-?dup if \ load file
-rom-kernal bufstart loadb
+filename c@ if \ load file
+rom-kernal
+
+\ Abort if the file is too big to load.
+'$' here c! ':' here 1+ c!
+filename 1+ here 2+ $f move
+here filename c@ 2+ here loadb drop
+here $22 + @ $2020 = \ found?
+here $20 + @ #44 > and \ 44=$2c00/254
+abort" too big"
+
+filename count bufstart loadb
 ?dup 0= if reset-buffer else
-eof ! 0 eof @ c! then
-else drop then main-loop ;
+eof ! 0 eof @ c! then then main-loop ;
 
 to latest \ end hiding words

docs/memmap.tex
@@ -9,16 +9,14 @@
 \item[\ldots]
 \item[\$100 - \$1ff] Return stack.
 \item[\$200 - \$258] Text input buffer.
-\item[\$33C - \$3FB] PAD Scratch pad memory, Cassette Buffer, untouched by DurexForth. 
+\item[\$33C - \$3FB] PAD Scratch pad memory, Cassette Buffer, untouched by DurexForth.
 \item[\ldots]
 \item[\$801 - \texttt{here}] Forth Kernel followed by code and data space.
 \item[\ldots]
-\item[\$7000 - \texttt{eof}] Editor text buffer. Grows upwards as needed.
-\item[\ldots]
 \item[\texttt{latest} - \$9fff] Dictionary. Grows downwards as needed.
-\item[\$a000 - \$cbff] Free RAM.
+\item[\$a000 - \$cbff] Editor text buffer.
 \item[\$cc00 - \$cfff] Hi-res graphics colors.
-\item[\$d000 - \$dfff] I/O area. 
+\item[\$d000 - \$dfff] I/O area.
 \item[\$e000 - \$ffff] Kernal / hi-res graphics bitmap.
 
 \end{description}

commit 310792d967f674e197fd87bb836bfe0112489bf9

fix V save. broke in 6ea629953f394f8230d36ddeff8488319b3a55c6

forth_src/v.fs
@@ -369,7 +369,7 @@ here
 's' over c! 1+
 '0' over c! 1+
 ':' over c! 1+
-filename count swap -rot move
+filename 1+ over filename c@ move
 filename c@ + lf swap c!
 here filename c@ 4 +
 $f $f open ioabort $f close