198d71f3317d2c5eb2559bbb66aadb0cb50c2804
Whammo <38296423+Whammo@users.noreply.github.com>
2022-01-05 21:44:26 -0800

    Create rs232.fs
     optional rs232 enabling NMI routines in durexForth Assembly

---
+++ forth_src/rs232.fs
@@ -0,0 +1,43 @@
+318 @ value old-vector
+
+
+code nmi
+here        pha,
+            txa,
+            pha,
+            tya,
+            pha,
+$7f         lda,#
+$dd0d       sta,
+$dd0d       ldy,
++branch     bpl,
+
+$fe72       jmp,
+
+here swap               \ +branch to tos         
+:+          pla,
+            pla,
+            tax,
+            sei,
+                        \ quit_reset
+swap dup 100/
+swap $ff and
+
+            lda,#       \ restore
+$318        sta,
+            lda,#
+$319        sta,
+
+dup 100/ swap
+$ff and
+ 
+            lda,#
+$316        sta,
+            lda,#
+$317        sta,
+
+$0c old-vector +
+             jmp,
+;code
+
+: install ' nmi $318 ! abort ;
