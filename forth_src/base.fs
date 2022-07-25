5c2b9733ecb879278c805021e3ebc9755e65e7f0
Johan Kotlinski <kotlinski@gmail.com>
2022-07-26 01:44:31 +0200

    unify "word not found" error handling

--- forth_src/base.fs
+++ forth_src/base.fs
@@ -2,10 +2,9 @@
 : nip swap drop ;
 : jmp, 4c c, ;
 : postpone bl word dup find ?dup 0= if
-count rvs type '?' emit cr abort then
-rot drop -1 = if [ ' literal compile,
-' compile, literal ] then compile,
-; immediate
+count notfound then rot drop -1 = if [
+' literal compile, ' compile, literal
+] then compile, ; immediate
 : ['] ' postpone literal ; immediate
 : [char] char postpone literal
 ; immediate
