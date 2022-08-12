7a28e38b303a4dbe6264cae10c64198ec0085d6a
Johan Kotlinski <kotlinski@gmail.com>
2022-08-12 10:09:56 +0200

    size optimize

--- forth_src/see.fs
+++ forth_src/see.fs
@@ -35,25 +35,25 @@ swap rot then ;
 : xt>nt ( xt -- nt | 0 )
 0 swap literal dowords drop ;
 
-: 5+ 5 + ;
+: 3+ 3 + ; : 4+ 4 + ; : 5+ 5 + ;
 
 : scan-0branch ( addr -- addr+5 )
-dup 3 + @ 2dup branch! \ src dst src
+dup 3+ @ 2dup branch! \ src dst src
 u> 0= if \ back
 #until type! then 5+ ;
 
 : skip-lits ( addr -- addr )
-3 + dup @ + 2+ ;
+3+ dup @ + 2+ ;
 
 : scan-loop ( addr -- addr+5 )
 \ correct #else to #leave
 5+ branchptr @ here ?do
-dup i 2+ @ = if #leave i 4 + ! then
+dup i 2+ @ = if #leave i 4+ ! then
 6 +loop ;
 
 : scan-jsr ( addr -- addr )
 dup 1+ @ case
-['] litc of 4 + endof
+['] litc of 4+ endof
 ['] lit of 5+ endof
 ['] lits of skip-lits endof
 ['] (?do) of 5+ endof
@@ -61,7 +61,7 @@ dup 1+ @ case
 ['] (+loop) of scan-loop endof
 ['] (of) of 5+ endof
 ['] 0branch of scan-0branch endof
-drop 3 + dup endcase ;
+drop 3+ dup endcase ;
 
 \ it's a while-repeat if...
 \ 1) it's a backjump (repeat)
@@ -72,8 +72,8 @@ drop 3 + dup endcase ;
 dup dup 1+ @ u> 0= if 0 exit then
 \ 0branch fwd dst?
 0 branchptr @ here ?do
-over 3 + i 2+ @ = if \ dst?
-#while i 4 + !
+over 3+ i 2+ @ = if \ dst?
+#while i 4+ !
 drop 1 leave then 6 +loop ;
 
 : scan-jmp ( addr -- addr )
@@ -87,7 +87,7 @@ here branchptr !
 >xt begin dup c@ case
 $20 of scan-jsr endof
 $4c of scan-jmp reached-end if
-drop exit then 3 + endof
+drop exit then 3+ endof
 $e8 of 1+ endof \ inx
 $60 of \ rts
 reached-end if
@@ -101,8 +101,8 @@ else ." ??" then space ;
 : print-0branch ( addr -- addr+5 )
 branchptr @ here do
 i @ over = if
-i 4 + @ 10 > if ." until "
-else i 4 + @ #while = if
+i 4+ @ 10 > if ." until "
+else i 4+ @ #while = if
 ." while " else ." if " then
 then unloop 5+ exit then
 6 +loop abort ;
@@ -115,8 +115,8 @@ over c@ emit 1 /string repeat
 
 : print-unloop ( addr -- addr+3 )
 \ if followed by a leave, skip
-3 + branchptr @ here do
-dup i @ = i 4 + @ #leave = and if
+3+ branchptr @ here do
+dup i @ = i 4+ @ #leave = and if
 unloop exit then 6 +loop ." unloop " ;
 
 : print-of ( addr -- addr+5 )
@@ -124,30 +124,30 @@ unloop exit then 6 +loop ." unloop " ;
 
 : print-jsr ( addr -- addr )
 dup 1 + @ case
-['] lit of 3 + dup @ . 2+ endof
-['] litc of 3 + dup c@ . 1+ endof
+['] lit of 3+ dup @ . 2+ endof
+['] litc of 3+ dup c@ . 1+ endof
 ['] lits of print-lits endof
-['] (do) of 3 + ." do " endof
+['] (do) of 3+ ." do " endof
 ['] (?do) of 5+ ." ?do " endof
 ['] (loop) of 5+ ." loop " endof
 ['] (+loop) of 5+ ." +loop " endof
 ['] (of) of print-of endof
 ['] 0branch of print-0branch endof
 ['] unloop of print-unloop endof
-print-xt 3 + dup
+print-xt 3+ dup
 endcase ;
 
 : remove-then ( addr -- )
 branchptr @ here do i 2+ @ over =
-i 4 + @ #if = and if
+i 4+ @ #if = and if
 0 i 2+ ! then 6 +loop drop ;
 
 : print-jmp ( addr -- addr )
 dup 1+ @ dup my-xt @ u< if print-xt
 else drop branchptr @ here do
-i @ over = if i 4 + @ case
+i @ over = if i 4+ @ case
 #else of ." else "
-dup 3 + remove-then endof
+dup 3+ remove-then endof
 #repeat of ." repeat " endof
 #again of ." again " endof
 #leave of ." leave " endof
@@ -159,7 +159,7 @@ abort endcase then 6 +loop then ;
 : print-to-branch ( addr -- addr )
 branchptr @ here ?do
 dup i 2+ @ = if
-i 4 + @ case
+i 4+ @ case
 #repeat of .begin endof
 #again of .begin endof
 #until of .begin endof
@@ -177,7 +177,7 @@ dup c@ $80 and if ." immediate " then
 print-to-branch dup c@ case
 $20 of print-jsr endof
 $4c of print-jmp reached-end if
-drop ';' emit cr exit then 3 + endof
+drop ';' emit cr exit then 3+ endof
 $e8 of ." drop " 1+ endof \ inx
 $60 of \ rts
 reached-end if
