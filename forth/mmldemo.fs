d81b3aca8b8dd0e17be9a04b44113c2ef449bb77
Johan Kotlinski <kotlinski@gmail.com>
2023-04-16 14:01:13 +0200

    simplify mml"

--- forth/mmldemo.fs
+++ forth/mmldemo.fs
@@ -4,10 +4,10 @@ require mml
 r> 1+ dup 2+ swap @ 2dup + 1- >r ;
 : mml" ( -- addr len )
 literal compile, here >r 0 ,
-begin source >in @ /string if 1 >in +!
-c@ dup '"' = if r> 2drop exit then
-c, 1 r@ +! else refill 2drop then again
-; immediate
+begin >in @ '"' parse
+tuck here swap move dup allot
+dup r@ +! >in @ rot - = while
+refill drop repeat r> drop ; immediate
 
 cr .( Frere Jaques)
 : frere-jaques
