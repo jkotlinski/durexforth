d8c8c1dca838f93e0c5b381a60832e084dd629b1
Johan Kotlinski <kotlinski@gmail.com>
2023-04-22 20:08:25 +0200

    drop least recently used buffer

--- forth/block.fs
+++ forth/block.fs
@@ -3,27 +3,43 @@
 \ buffer block id's
 create bbi 0 , 0 c,
 
+\ last-used timestamps
+variable time
+create lu 0 , 0 , 0 ,
+
 create path 'b' c, 0 ,
 
-: >addr ( slot -- addr )
+\ updates last-used timestamp
+: touch ( buf -- buf )
+time @ over 2* lu + !
+1 time +! ;
+
+: >addr ( buf -- addr )
 $400 * $c000 + ;
 
-: doload ( n slot -- addr )
+: doload ( n buf -- addr )
 2dup bbi + c! >addr >r 10 /mod
 '0' + path 1+ c!
 '0' + path 2+ c!
 path 3 r@ loadb drop r> ;
 
-: block ( n -- addr )
-\ if already loaded, return buffer addr
+: already-loaded ( n -- addr|0 )
 3 0 do dup bbi i + c@ = if
-drop i >addr unloop exit
-then loop
+drop i touch >addr unloop exit
+then loop 0 ;
 
-\ load to an unused buffer, if possible
+: load-to-unused ( n -- addr|0 )
 3 0 do bbi i + c@ 0= if
-i doload unloop exit
-then loop
+i touch doload unloop exit
+then loop 0 ;
 
-\ discard a buffer and load it there
-;
+: drop-lru ( -- )
+0 lu @ lu 2+ @ <
+lu @ lu 4 + @ < and if 0
+else lu 2+ @ lu 4 + @ < if
+1 else 2 then then bbi + c! ;
+
+: block ( n -- addr )
+already-loaded ?dup if exit then
+load-to-unused ?dup if exit then
+drop-lru load-to-unused ;
