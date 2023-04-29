429c8df9b76b65727fb3c838e95f5f56e88d6acc
Johan Kotlinski <kotlinski@gmail.com>
2023-04-29 08:32:27 +0200

    added TODO comment

--- forth/block.fs
+++ forth/block.fs
@@ -15,7 +15,11 @@ variable map 0 map !
 <# 0 #s bl hold 2drop 0 #s bl hold
 '0' hold bl hold 'a' hold '-' hold
 'b' hold #> $f $f open ioabort
-$f chkin ioabort chrin begin chrin emit
+$f chkin ioabort
+\ TODO: get next free track/sector
+\ from the error string. as is,
+\ it is way too slow.
+chrin begin chrin emit
 readst until clrchn $f close '0' = ;
 
 \ Usage: "20 create-blocks" allocates
