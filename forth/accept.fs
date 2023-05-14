c5b76eda58eae16d05bcf90b354c6dd690ed68be
ekipan <nmessenger@gmail.com>
2023-05-14 19:13:26 -0400

    (trivial) whitespace, comments, code order

--- forth/accept.fs
+++ forth/accept.fs
@@ -1,13 +1,12 @@
 0 value addr
 : accept ( addr avail -- len )
+0 $cc ( enable cursor-blink ) c!
 swap to addr 0 ( avail len )
-0 $cc c! \ cursor on
 begin key case
-$d of \ cr
- 1 $cc c! nip space exit endof
-$14 of \ del
- dup if 1- $14 emit then endof
-( avail len char ) \ add to buffer?
+\ return and delete:
+$0d of nip space 1 $cc c! exit endof
+$14 of dup if 1- $14 emit then endof
+\ ( avail len char ) add to buffer?
 >r 2dup > r@ $7f and $1f > and if
  r@ over addr + c! 1+ r@ emit then r>
 endcase again ;
