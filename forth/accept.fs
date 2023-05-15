c1fa52f7bf4888107fa70ee023b422fe8da6399f
ekipan <nmessenger@gmail.com>
2023-05-15 12:46:29 -0400

    space-optimize `accept` (#557)
    * space-optimize `accept`
    
    * allow typing uppercase (whoops).
    
    * (trivial) whitespace, comments, code order

--- forth/accept.fs
+++ forth/accept.fs
@@ -1,24 +1,13 @@
-variable curr
-: accept ( addr u -- u )
-$cc >r 0 $cc c! \ enable cursor
-swap dup >r curr !
-begin
- key
- dup $d = if \ cr
-  2drop curr @ r> -
-  space r> $cc c! \ reset cursor
-  exit
- else dup $14 = if \ del
-  curr @ r@ > if
-   emit -1 curr +! 1+
-  else drop then
- else dup $7f and $20 < if
-  drop \ ignore
- else
-  \ process character
-  over if dup curr @ c!
-   emit 1- 1 curr +!
-  else drop then
- then then then
-again ;
-hide curr
+0 value addr
+: accept ( addr avail -- len )
+0 $cc ( enable cursor-blink ) c!
+swap to addr 0 ( avail len )
+begin key case
+\ return and delete:
+$0d of nip space 1 $cc c! exit endof
+$14 of dup if 1- $14 emit then endof
+\ ( avail len char ) add to buffer?
+>r 2dup > r@ $7f and $1f > and if
+ r@ over addr + c! 1+ r@ emit then r>
+endcase again ;
+hide addr
